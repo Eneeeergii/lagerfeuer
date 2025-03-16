96b9a551bdcf25683d4e630278e95fba896252f4
Eneeeergii <Azad.Cicek@outlook.com>
2025-03-16 17:48:36 +0100

    wrong env variable

--- kubernetes/install_kubernetes_firstnode.sh
+++ kubernetes/install_kubernetes_firstnode.sh
@@ -58,7 +58,6 @@ sed -i "s/127.0.0.1/${K3S_API_IP}/g" "$KUBECONFIG_FILE"
 echo "✅ K3s kubeconfig updated to use ${K3S_API_IP}"
 
 # --- Add further Master Nodes ---
-export INSTALL_K3S_EXEC="$ADD_K3S_MASTER"
 
 if [ "$HA_CLUSTER" == "true" ]; then
     IFS=',' read -r -a MASTER_NODES <<< "$MASTERS"
@@ -67,9 +66,8 @@ if [ "$HA_CLUSTER" == "true" ]; then
 
     for master in "${MASTER_NODES[@]}"; do
         echo "🚀 Installing K3s version $K3S_VERSION on $master and adding it to the K3s Cluster"
-        ssh -i "$SSH_KEY" "$SSH_USER@$master" "export $INSTALL_K3S_EXEC && export $HA_CLUSTER"
         ssh -i "$SSH_KEY" "$SSH_USER@$master" << EOF
-curl -sfL https://get.k3s.io | sh -
+curl -sfL https://get.k3s.io | K3S_TOKEN=$K3S_TOKEN sh -s - server --server https://$K3S_API_IP:6443 --write-kubeconfig-mode 644
 EOF
         echo "✅ K3s installed and $master is added to K3s Cluster!"
     done
