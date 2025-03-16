f74f5fd23b811083a6562bc9158af7c5c78c6e90
Eneeeergii <Azad.Cicek@outlook.com>
2025-03-16 21:58:55 +0100

    last changes

---
+++ kubernetes/functions/remove_user_pub_key.sh
@@ -0,0 +1,15 @@
+remove_sudoers_permission_remote(){
+
+    HOST=$1
+    USER=$2
+    userdel k3s-install $USER
+
+}
+
+remove_sudoers_permission_local(){
+
+    HOST=$1
+    USER=$2
+    userdel k3s-install $USER
+
+}
