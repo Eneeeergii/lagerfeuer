a1d266315d9c54f767f3b65a74712881f2378497
Eneeeergii <Azad.Cicek@outlook.com>
2025-03-18 17:50:28 +0100

    added delete of CRDs

--- kubernetes/apps/postgresql/delete_operator.sh
+++ kubernetes/apps/postgresql/delete_operator.sh
@@ -4,3 +4,5 @@ kubectl delete ns psql
 kubectl delete ClusterRole postgres-operator
 kubectl delete ClusterRole postgres-pod
 kubectl delete ClusterRoleBinding postgres-operator
+kubectl delete customresourcedefinition postgresqls.acid.zalan.do
+kubectl delete customresourcedefinition postgresteams.acid.zalan.do
