421e57add59d13c35e44fcfccaee659460598b32
Eneeeergii <Azad.Cicek@outlook.com>
2025-03-18 12:41:35 +0100

    added new function

--- kubernetes/functions/install_postgresql_operator.sh
+++ kubernetes/functions/install_postgresql_operator.sh
@@ -2,12 +2,24 @@ set -e
 
 install_postgresql_operator(){
 
+    CONFIG_FILE=$1
+
+    if [ ! -f "$CONFIG_FILE" ]; then
+        echo "❌ Configuration file '$CONFIG_FILE' not found!"
+        exit 1
+    fi
+
+    source "$CONFIG_FILE"
+    echo "✅ Loaded configuration from $CONFIG_FILE"
+
     #Check if Operator should be installed
-    if [ "$HA_CLUSTER" == "true" ]; then
-        if [ -z "$MASTERS" ]; then
-            echo "❌ No MASTERS are set!"
+    if [ "$POSTGRESQL_OPERATOR_INSTALL" == "true" ]; then
+        if [ -z "$POSTGRESQL_NAMESPACE" ]; then
+            echo "❌ K3S_VERSION is not set!"
             exit 1
         fi
+    else
+
     fi
 
 }
