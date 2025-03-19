733e6dc1fb161464f63c9f79f3b75d27d82da624
Eneeeergii <Azad.Cicek@outlook.com>
2025-03-19 18:56:05 +0100

    changed parameter vlaues for testing installation script for kubevipLB

--- kubernetes/functions/install_kubeVIP_lb.sh
+++ kubernetes/functions/install_kubeVIP_lb.sh
@@ -21,13 +21,10 @@ install_kubeVIP_cloud_provider_on_prem(){
 
     if [ "$DEPLOY_LB_KUBEVIP" == "true" ]; then
 
-        
-       
         envsubst < "$KUBE_VIP_LB_YAML" | kubectl apply -f -
         envsubst < "$KUBE_VIP_CLOUD_PROVIDER_CONFIGMAP_YAML" | kubectl apply -f -
         envsubst < "$KUBE_VIP_CLOUD_PROVIDER_YAML" | kubectl apply -f -
-        
-        
+          
     elif [ "$DEPLOY_LB_KUBEVIP" == "false" ]; then
 
         echo "⚙️"
