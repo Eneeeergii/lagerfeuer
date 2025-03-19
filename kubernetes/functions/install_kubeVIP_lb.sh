e4e733e5327a552128720ebc33fd0ba7d30b96ad
Eneeeergii <Azad.Cicek@outlook.com>
2025-03-19 18:54:02 +0100

    added function to install kubevip as LB

---
+++ kubernetes/functions/install_kubeVIP_lb.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+set -e
+
+#Enable this and the function calls at the bottom, to test this script isolated
+CONFIG_FILE=/home/k3s-install/lagerfeuer/kubernetes/config.env
+source "$CONFIG_FILE"
+echo "✅ Loaded configuration from $CONFIG_FILE"
+
+check_parameters_kubevip(){
+
+    if [ -z "$VIP_LB_RANGE" ]; then
+        echo "❌ NAMESPACE is not set!"
+        exit 1
+    fi
+
+}
+
+install_kubeVIP_cloud_provider_on_prem(){
+
+
+
+    if [ "$DEPLOY_LB_KUBEVIP" == "true" ]; then
+
+        
+       
+        envsubst < "$KUBE_VIP_LB_YAML" | kubectl apply -f -
+        envsubst < "$KUBE_VIP_CLOUD_PROVIDER_CONFIGMAP_YAML" | kubectl apply -f -
+        envsubst < "$KUBE_VIP_CLOUD_PROVIDER_YAML" | kubectl apply -f -
+        
+        
+    elif [ "$DEPLOY_LB_KUBEVIP" == "false" ]; then
+
+        echo "⚙️"
+
+    else
+
+    fi
+
+}
+
+install_kubeVIP_cloud_provider_on_prem
