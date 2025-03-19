df3e2413c8ee1bf22661e08318969852f55c726b
Eneeeergii <Azad.Cicek@outlook.com>
2025-03-20 00:24:41 +0100

    function to check if volume group exists

---
+++ kubernetes/functions/check_volume_group.sh
@@ -0,0 +1,12 @@
+check_volume_group(){
+
+    VG_NAME=$1
+
+    if sudo vgdisplay "$VG_NAME" &> /dev/null; then
+        echo "✅ Volume Group '$VG_NAME' exists."
+    else
+        echo "❌ Error: Volume Group '$VG_NAME' does not exist. Please create Volume Group and write it into config.env file!"
+        exit 1
+    fi
+
+}
