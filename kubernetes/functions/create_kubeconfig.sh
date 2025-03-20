cde3111ac342b9893ee838ee2e872320018a5079
Eneeeergii <Azad.Cicek@outlook.com>
2025-03-20 13:32:08 +0100

    created new function for changing kubeconfig

---
+++ kubernetes/functions/create_kubeconfig.sh
@@ -0,0 +1,44 @@
+create_ha_kubeconfig(){
+
+    ORIGINAL_KUBECONFIG=$1
+    HA_KUBECONFIG=$2
+    API_IP=$3
+    NEW_URL="https://$API_IP:6443"
+
+    echo $NEW_URL
+
+
+    # Check if Kubeconfig exists
+    if [ ! -f "$ORIGINAL_KUBECONFIG" ]; then
+        echo "❌ Error: Original kubeconfig file not found at $ORIGINAL_KUBECONFIG"
+        exit 1
+    fi
+
+    # Copy and rename config
+    cp "$ORIGINAL_KUBECONFIG" "$HA_KUBECONFIG"
+
+    # Set new Server URL
+    sed -i "s|https://127.0.0.1:6443|$NEW_URL|g" "$HA_KUBECONFIG"
+
+    # Verify changed URL
+    if grep -q "$NEW_URL" "$HA_KUBECONFIG"; then
+        echo "✅ Successfully updated server URL in $HA_KUBECONFIG"
+    else
+        echo "❌ Error: Failed to update server URL"
+        exit 1
+    fi
+
+    # Set the new KUBECONFIG in /etc/environment
+    if grep -q "KUBECONFIG=" /etc/environment; then
+        sudo sed -i "s|KUBECONFIG=.*|KUBECONFIG=$HA_KUBECONFIG|" /etc/environment
+    else
+        echo "KUBECONFIG=$HA_KUBECONFIG" | sudo tee -a /etc/environment
+    fi
+
+    # Apply the new environment variable
+    export KUBECONFIG="$HA_KUBECONFIG"
+
+    echo "✅ New KUBECONFIG set to $HA_KUBECONFIG"
+    echo "⚠️ Run 'source /etc/environment' or restart your session to apply the changes."
+
+}
