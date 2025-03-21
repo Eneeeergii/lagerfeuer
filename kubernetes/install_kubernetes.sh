4ccba9fe14c85a1fe4d0c86fe64feb6e0ab54b48
Eneeeergii <Azad.Cicek@outlook.com>
2025-03-21 15:27:52 +0100

    testing

--- kubernetes/install_kubernetes.sh
+++ kubernetes/install_kubernetes.sh
@@ -15,11 +15,11 @@ source "$CONFIG_FILE"
 echo "✅ Loaded configuration from $CONFIG_FILE"
 
 # Include all functions
-#for file in ./functions/*.sh; do
-#    if [ -f "$file" ]; then
-#        source "$file"
-#    fi
-#done
+for file in ./functions/*.sh; do
+    if [ -f "$file" ]; then
+        source "$file"
+    fi
+done
 
 #Check Output of Variables
 #echo $KUBE_VIP_API_YAML
