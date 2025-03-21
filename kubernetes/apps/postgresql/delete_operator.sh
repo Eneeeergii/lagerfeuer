ba2707788d1c8bebe4e18a4bfc5184bf57abe029
Eneeeergii <Azad.Cicek@outlook.com>
2025-03-21 21:12:10 +0100

    not needed anymore

--- kubernetes/apps/postgresql/delete_operator.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#For testing reason: delete Operator and all dependencies
-kubectl delete ns psql
-kubectl delete ClusterRole postgres-operator
-kubectl delete ClusterRole postgres-pod
-kubectl delete ClusterRoleBinding postgres-operator
-kubectl delete customresourcedefinition postgresqls.acid.zalan.do
-kubectl delete customresourcedefinition postgresteams.acid.zalan.do
